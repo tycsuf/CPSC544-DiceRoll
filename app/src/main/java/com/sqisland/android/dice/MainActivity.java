4ddd8a4cfe735786228dbe233efe7d3b6f543152
tyau <tyau@proteus.com>
2018-09-25 22:23:36 -0700

    Added main activity, ability to open application, basic hello world display

---
+++ app/src/main/java/com/sqisland/android/dice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sqisland.android.dice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+  }
+
+}
