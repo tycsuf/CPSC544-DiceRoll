3b78e960a37e9f4afd72e910174e1620f52736ad
tyau <tyau@proteus.com>
2018-09-30 23:14:48 -0700

    Fixed app exit

--- app/src/main/java/com/sqisland/android/dice/MainActivity.java
+++ app/src/main/java/com/sqisland/android/dice/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
     setContentView(R.layout.activity_main);
 
     if (getIntent().getBooleanExtra("Exit me", false)) {
-      finish();
+      finishAndRemoveTask();
     }
 
     resultView = (TextView) findViewById(R.id.result);
